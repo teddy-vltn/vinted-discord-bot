4de2fb16bf7ab911f80e1bcdd2d74245d3189778
tvalenti <teddy.valentin@etudiant.univ-lr.fr>
2024-05-01 16:26:38 +0200

    comments

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 import VintedMonitoringService from "./src/services/vinted_monitoring_service.js";
 import Logger from "./src/utils/logger.js";
 
+/*
+    This is a showcase of the VintedMonitoringService class.
+    The service is used to monitor a Vinted catalog page and notify the user when new items are added.
+
+    You can run it, but this is not linked with any bot
+    The monitoring service will log the new items to the console.
+
+    You can build around it and send whatever notifications you want to wherever 
+    you want when new items are found.
+*/
+
 const monitoringService = new VintedMonitoringService();
 
 monitoringService.startMonitoring(
