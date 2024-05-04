3b7ff44b237c9929abdcad11358f61fe60a10532
tvalenti <teddy.valentin@etudiant.univ-lr.fr>
2024-05-04 18:04:51 +0200

    revert cache changes

--- src/monitor/vinted_monitor.js
+++ src/monitor/vinted_monitor.js
@@ -1,14 +1,17 @@
+const VINTED_API_URL = "https://www.{domain}/api/v2/catalog"
+
 import { UrlTransformer } from "../utils/url_transformer.js";
 import Logger from "../utils/logger.js";
 
-export default class VintedMonitor {
-    constructor(handler, proxyHandler = null) {
-        this.handler = new handler(proxyHandler);
+class VintedMonitor {
+    constructor(handler, proxy_handler = null) {
+        this.handler = new handler(proxy_handler=proxy_handler);
         this.interval = null;
+        this.lastItemIds = new Set(); // Cache to store the IDs of previously fetched items
         this.firstRun = true;
     }
 
-    startMonitoring(url, processItems, interval) {
+    startMonitoring(url, callback, interval) {
         const apiUrl = UrlTransformer.transform(url);
         if (!apiUrl) {
             Logger.error("Invalid URL");
@@ -19,8 +22,20 @@ export default class VintedMonitor {
         this.interval = setInterval(async () => {
             try {
                 const items = await this.handler.fetchItems(apiUrl);
-                processItems(items, this.firstRun);
-                this.firstRun = false; // Set firstRun to false after the first check
+                const newItems = items.filter(item => !this.lastItemIds.has(item.id));
+                // sort by highest id last to get the newest items first
+                Logger.debug(`New items found: ${newItems.length}`);
+                newItems.sort((a, b) => a.id - b.id);
+                if (newItems.length > 0) {
+                    newItems.forEach(item => this.lastItemIds.add(item.id)); // Update cache
+                    if (this.firstRun) {
+                        this.firstRun = false;
+                        Logger.info(`First run, skipping ${newItems.length} items`);
+                        Logger.info(`This is the first run, skipping ${newItems.length} items`);
+                    } else {
+                        callback(newItems);
+                    }
+                }
             } catch (error) {
                 Logger.warn(`Error fetching items: ${error.message}, ${error.stack}`);
             }
@@ -31,3 +46,5 @@ export default class VintedMonitor {
         if (this.interval) clearInterval(this.interval);
     }
 }
+
+export default VintedMonitor;
--- src/services/vinted_monitoring_service.js
+++ src/services/vinted_monitoring_service.js
@@ -4,13 +4,14 @@ import ProxyHandler from "../handlers/proxy_handler.js";
 import ConfigurationManager from "../utils/config_manager.js";
 import Logger from "../utils/logger.js";
 
-export default class VintedMonitoringService {
+class VintedMonitoringService {
     constructor() {
         this.configManager = new ConfigurationManager(process.cwd() + '/config.yaml');
         const proxies = this.configManager.getProxies();
+
         const isProxyEnabled = this.configManager.isProxyEnabled();
+
         this.proxyHandler = null;
-        this.lastItemIds = new Set();
 
         if (isProxyEnabled && proxies.length === 0) {
             Logger.error("Proxies are enabled but none are configured.");
@@ -21,26 +22,22 @@ export default class VintedMonitoringService {
             try {
                 this.proxyHandler = new ProxyHandler(proxies);
                 Logger.info("Proxy handler created, using proxies for requests.");
-            } catch (error) {
+            }
+            catch (error) {
                 Logger.error(`Error creating proxy handler: ${error.message}`);
                 return null;
             }
         }
+
     }
 
     startMonitoring(url, callback) {
         const interval = this.configManager.getInterval();
-        const processItems = (items, firstRun) => {
-            const newItems = items.filter(item => !this.lastItemIds.has(item.id));
-            newItems.forEach(item => this.lastItemIds.add(item.id));
-            
-            if (!firstRun && newItems.length > 0) {
-                callback(newItems);
-            }
-        };
-
+        
         const vintedMonitor = new VintedMonitor(VintedHandlerAPIBasic, this.proxyHandler);
-        vintedMonitor.startMonitoring(url, processItems, interval);
+        vintedMonitor.startMonitoring(url, callback, interval);
         return vintedMonitor;
     }
 }
+
+export default VintedMonitoringService;
