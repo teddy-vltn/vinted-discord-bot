c577ebe93a95a745de68a81e8cee98ce55bfaa7f
tvalenti <teddy.valentin@etudiant.univ-lr.fr>
2024-05-05 15:24:28 +0200

    avoid null values in VintedItem

--- src/entities/vinted_item.js
+++ src/entities/vinted_item.js
@@ -14,17 +14,16 @@ class VintedItem {
 
     static fromApiResponse(apiResponse) {
         return new VintedItem({
-            id: apiResponse.id,
-            title: apiResponse.title,
-            price: apiResponse.price,
-            size_title: apiResponse.size_title,
-            url: apiResponse.url,
-            photo_url: apiResponse.photo.url,
-            profile_url: apiResponse.user.profile_url,
-            brand_title: apiResponse.brand_title,
-            status: apiResponse.status,
-            service_fee: apiResponse.service_fee
-
+            id: apiResponse.id || "N/A",
+            title: apiResponse.title || "N/A",
+            price: apiResponse.price || "N/A",
+            size_title: apiResponse.size_title || "N/A",
+            url: apiResponse.url || "N/A",
+            photo_url: apiResponse.photo.url || "https://www.vinted.it/assets/no-photo/medium-8d7f",
+            profile_url: apiResponse.user.profile_url || "https://www.vinted.it/member/12345678",
+            brand_title: apiResponse.brand_title || "N/A",
+            status: apiResponse.status || "N/A",
+            service_fee: apiResponse.service_fee || "N/A"
         });
     }
 }
